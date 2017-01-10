37e75f39654305439b7fcc35098d70dbdd5f98a7
Oleg Makarov <Oleg.Makarov@sophos.com>
2017-01-10 12:30:18 +0200

    01 02

---
+++ 01-box.js
@@ -0,0 +1,24 @@
+// const nextCharForNumberString = str => {
+//   const trimmed = str.trim()
+//   const number = parseInt(trimmed)
+//   const nextNumber = number + 1
+//   return String.fromCharCode(nextNumber)
+// }
+
+const Box = x =>
+({
+  map:      f => Box(f(x)),
+  fold:     f => f(x),
+  inspect: () => `Box(${x})`
+})
+
+const nextCharForNumberString = str =>
+  Box(str)
+  .map(s => s.trim())
+  .map(s => parseInt(s))
+  .map(i => i + 1)
+  .fold(i => String.fromCharCode(i))
+
+const result = nextCharForNumberString(' 64 ')
+
+console.log(result);
